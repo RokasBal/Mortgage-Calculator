41014dfdb94d873c45cff682be5c3c8b90c1f0fc
Rokas Baliutavičius <baliutaviciusrokas@gmail.com>
2024-03-01 18:32:35 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.mortgagecalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.mortgagecalculator to javafx.fxml;
+    exports com.example.mortgagecalculator;
+}
